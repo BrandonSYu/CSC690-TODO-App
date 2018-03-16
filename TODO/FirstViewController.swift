fa51ee7c4b16f37ce8d1741d04b5bbd1ccba3d06
Brandon <beyu@Brandons-Mac.local>
2018-03-16 01:45:39 -0700

    sample test. add/delete works. add task in separate view. need to implement multiple column cell to display a tappable on/off mark in the form of a picture. need edit task.

--- TODO/FirstViewController.swift
+++ TODO/FirstViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var todolist = ["Get Food", "Mail", "Grocery Shopping"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(todolist.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        tableCell.textLabel?.text=todolist[indexPath.row]
+        return (tableCell)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            todolist.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
